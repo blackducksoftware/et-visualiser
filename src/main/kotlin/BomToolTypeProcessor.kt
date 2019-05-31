278f0fcb557f255f28c80509b0d3c72850a4f311
Jordan Piscitelli <jordanp@synopsys.com>
2019-05-31 12:56:17 -0400

    Moving meta data to a pre-processor.

--- src/main/kotlin/BomToolTypeProcessor.kt
+++ src/main/kotlin/BomToolTypeProcessor.kt
@@ -3,38 +3,23 @@ import com.google.api.services.analyticsreporting.v4.model.GetReportsResponse
 
 data class BomToolStat(val type: String, val count: Int)
 
-
-data class Dimension(val bomToolTypes: String)
-
 class BomToolTypeProcessor {
     fun processResponse(structuredAnalytics: StructuredAnalytics): Collection<BomToolStat> {
         val bomToolMap = mutableMapOf<String, Int>()
+        val metaDataPreProcessor = MetaDataPreProcessor()
 
         for (analytic in structuredAnalytics.analytics) {
             if (analytic.dimensions.containsKey(CustomDimensions.META_DATA.dimensionName)){
-                val metadataPayload = analytic.dimensions.get(CustomDimensions.META_DATA.dimensionName);
-                if (metadataPayload != null && metadataPayload.contains("bomToolTypes")) {
-                    val dimension = Klaxon().parse<Dimension>(metadataPayload)
-                    dimension?.bomToolTypes?.split(",")?.forEach {
-                        var count = analytic.metrics.get("sessions")?.toInt() ?: 0;
-
-                        var key = it.trim()
-                        if (key.contains(":")) {
-                            // Earlier versions of Detect would submit the count in a different format
-                            key = key.split(":")[0]
+                val metadataPayload = analytic.dimensions.get(CustomDimensions.META_DATA.dimensionName)!!
+                val metaData = metaDataPreProcessor.preprocessMetaData(metadataPayload)
+                if (metaData != null){
+                    for (type in metaData.detectors){
+                        var metric = analytic.metrics.get("sessions")?.toInt()!!
+                        if (bomToolMap.containsKey(type)){
+                            bomToolMap[type] = bomToolMap[type]!! + metric
+                        } else {
+                            bomToolMap[type] = metric
                         }
-                        key = key.trim()
-
-                        if (key.contentEquals("")) {
-                            key = "None"
-                        }
-
-
-                        val value = bomToolMap[key]
-                        if (value != null) {
-                            count += value
-                        }
-                        bomToolMap[key] = count
                     }
                 }
             }
---
+++ src/main/kotlin/MetaDataProcessor.kt
@@ -0,0 +1,35 @@
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Klaxon
+import com.beust.klaxon.lookup
+import com.google.api.services.analyticsreporting.v4.model.GetReportsResponse;
+import java.io.StringReader
+
+class MetaDataPreProcessor {
+
+    fun preprocessMetaData(metadataJson: String): MetaData? {
+
+        val json = Klaxon().parseJsonObject(StringReader(metadataJson))
+        val detectors = mutableListOf<String>()
+        val detectorTimings = mutableMapOf<String, String>()
+
+        if (json.containsKey("bomToolTypes")){
+            val allBomToolTypes:String = json.string("bomToolTypes")!!
+
+            val bomToolTypes = allBomToolTypes.split(",")
+            for (type in bomToolTypes){
+                var pieces = type.split(":")
+                if (pieces.size == 1){
+                    detectors.add(pieces[0]);
+                } else if (pieces.size == 2) {
+                    detectors.add(pieces[0]);
+                    detectorTimings[pieces[0]] = pieces[1]
+                }
+            }
+        }
+
+        return MetaData(detectors, detectorTimings)
+    }
+}
+
+data class MetaData (val detectors: List<String>, val detectorTimings: Map<String, String>)
+
